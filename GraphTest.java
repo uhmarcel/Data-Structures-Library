dc99fe92653edbbd8392a09528ad771baa7d80d8
Marcel Riera <mrier011@fiu.edu>
2019-12-09 14:38:05 -0500

    Added implementation of Bellman-Ford shortest path distance algorithm

--- GraphTest.java
+++ GraphTest.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import structures.Graph;
 
 
@@ -7,15 +8,15 @@ public class GraphTest {
     public static void main(String[] args) {
             
         Graph g = new Graph(8);
-        g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(0, 3);
-        g.addEdge(2, 4);
-        g.addEdge(3, 5);
-        g.addEdge(4, 5);
-        g.addEdge(5, 6);
-        g.addEdge(1, 7);
-        g.addEdge(6, 7);
+        g.addEdge(0, 1, 2);
+        g.addEdge(0, 2, 1);
+        g.addEdge(0, 3, 1);
+        g.addEdge(2, 4, 3);
+        g.addEdge(3, 5, 2);
+        g.addEdge(4, 5, 2);
+        g.addEdge(5, 6, 5);
+        g.addEdge(1, 7, 5);
+        g.addEdge(6, 7, 1);
         
         Graph.printGraph(g);
         
@@ -23,6 +24,14 @@ public class GraphTest {
         System.out.println("BFS: " + g.BFS(s));
         System.out.println("DFS: " + g.DFS(s));
         System.out.println("Topological: " + g.topologicalSort());
+        
+        int[][] sp = g.BellmanFordSPD(s);
+        System.out.println("Bellman-Ford Shortest Path:");
+        System.out.println("  Dist[v] ->" + Arrays.toString(sp[Graph.DISTANCE_INDEX]));
+        System.out.println("  Prev[v] ->" + Arrays.toString(sp[Graph.PREVIOUS_INDEX]));
+        
+        
+//        g.MST();
         
     }
 }
--- structures/Graph.java
+++ structures/Graph.java
@@ -1,24 +1,41 @@
 package structures;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
 public class Graph {
     
-    public class WeightedPair {
+    public class Node implements Comparable<Node> {        
         public int u;
         public int w;
-        public WeightedPair(int u, int w) {
+        
+        public Node(int u, int w) {
             this.u = u;
             this.w = w;
         }
+
+        @Override
+        public int compareTo(Node t) {
+            return this.w - t.w;
+        }
+        
+        @Override
+        public String toString() {
+            return Integer.toString(u) + "-" + w;
+        }
     }
+        
+    public static final int DISTANCE_INDEX = 0;
+    public static final int PREVIOUS_INDEX = 1;
     
     private int vertices;
-    private List<WeightedPair>[] adjacencyList;
+    private List<Node>[] adjacencyList;
 
     public Graph(int vertices) {
         this.vertices = vertices;
@@ -34,7 +51,7 @@ public class Graph {
     }
     
     public void addEdge(int v, int u, int w) {
-        this.adjacencyList[v].add(new WeightedPair(u,w));
+        this.adjacencyList[v].add(new Node(u,w));
     }
     
     public void addEdgeUndirected(int v, int u) {
@@ -46,7 +63,7 @@ public class Graph {
         return this.vertices;
     }
     
-    public List<WeightedPair> getAdjacent(int vertex) {
+    public List<Node> getAdjacent(int vertex) {
         return this.adjacencyList[vertex];
     }
     
@@ -72,7 +89,7 @@ public class Graph {
                 result.add(v);
             }
             
-            for (WeightedPair edge : this.adjacencyList[v]) {
+            for (Node edge : this.adjacencyList[v]) {
                 if (!visited[edge.u]) {
                     Q.add(edge.u);
                 }
@@ -102,7 +119,7 @@ public class Graph {
                 result.add(v);
             }
             
-            for (WeightedPair edge : this.adjacencyList[v]) {
+            for (Node edge : this.adjacencyList[v]) {
                 if (!visited[edge.u]) {
                     S.push(edge.u);
                 }
@@ -125,13 +142,53 @@ public class Graph {
     private void topologicalSortHelper(int v, boolean[] visited, LinkedList<Integer> result) {
         visited[v] = true;
         
-        for (WeightedPair edge : this.adjacencyList[v]) {
+        for (Node edge : this.adjacencyList[v]) {
             if (!visited[edge.u]) {
                 topologicalSortHelper(edge.u, visited, result);
             }
         }
         
         result.addFirst(v);
+    }
+    
+    
+    // Incomplete
+    public Graph MST() {                
+        PriorityQueue<Node> Q = new PriorityQueue();
+        
+        for (int v = 0; v < this.vertices; v++) {
+            for (Node e : this.adjacencyList[v]) {
+                Q.add(e);
+            }
+        }
+        
+        while (!Q.isEmpty()) {
+            System.out.println(Q.remove());
+        }
+        return null;
+    }
+    
+    public int[][] BellmanFordSPD(int s) {
+        int[] dist = new int[vertices];
+        int[] prev = new int[vertices];
+        
+        for (int v = 0; v < vertices; v++) {
+            dist[v] = Integer.MAX_VALUE;
+            prev[v] = -1;
+        }
+    
+        dist[s] = 0;
+        
+        for (int v = 0; v < vertices - 1; v++) {
+            for (Node e : adjacencyList[v]) {
+                if (dist[e.u] > dist[v] + e.w) {
+                    dist[e.u] = dist[v] + e.w;
+                    prev[e.u] = v;
+                }
+            }
+        }
+        
+        return new int[][] {dist, prev};
     }
     
     
@@ -150,7 +207,7 @@ public class Graph {
         int size = g.getVertexAmount();
         
         for (int v = 0; v < size; v++) {
-            for (WeightedPair edge : g.getAdjacent(v)) {
+            for (Node edge : g.getAdjacent(v)) {
                 System.out.print(edge.u + ", ");
             }
             System.out.println();
